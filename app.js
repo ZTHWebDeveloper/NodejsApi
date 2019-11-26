53e82403eeb5be189b9848f598099369cfbf1e50
Ei Maung <eimg@fairwayweb.com>
2019-11-26 17:20:58 +0630

    Change mongodb url

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 const port = 8500;
 app.use(bodyParser.json());//If for API,it is used.
 app.use(productRoute);//productRoute
-mongoose.connect('mongodb+srv://naungnaung:PKs7mWuztwPHfRQ7@cluster0-qmjxz.mongodb.net/nodeapi?retryWrites=true&w=majority')
+mongoose.connect('mongodb+srv://naungnaung:PKs7mWuztwPHfRQ7@cluster0-qmjxz.mongodb.net/nodeapi')//PKs7mWuztwPHfRQ7
 .then(connected =>{
     app.listen(port,() =>{
         console.log('Server is running!! '+port);
