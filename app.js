624c02e38f5bb5ff750e193fb240eac9667b7d7e
Ei Maung <eimg@fairwayweb.com>
2019-11-27 13:46:51 +0630

    make it better

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ const productRoute = require('./routes/product')//product Route
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const app = express();
-const port = 8500;
+const port = 5000;
 app.use(bodyParser.json());//If for API,it is used.
 app.use(productRoute);//productRoute
 mongoose.connect('mongodb+srv://naungnaung:PKs7mWuztwPHfRQ7@cluster0-qmjxz.mongodb.net/nodeapi')//PKs7mWuztwPHfRQ7
 .then(connected =>{
-    app.listen(port,() =>{
+    app.listen(process.env.port,() =>{
         console.log('Server is running!! '+port);
     }); 
 })
